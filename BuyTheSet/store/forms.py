eec50541c9f79accdb0d9958cca45adb6b66c651
S-H-U-H-A-R-I <triston.c.0186@gmail.com>
2024-04-21 20:54:39 +0200

    added a class to update the user profile

--- BuyTheSet/store/forms.py
+++ BuyTheSet/store/forms.py
@@ -3,6 +3,24 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+class UpdateUserform(UserChangeForm):
+	password = None
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email')
+
+	def __init__(self, *args, **kwargs):
+		super(UpdateUserform, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'User Name'})
+		self.fields['username'].label = ''
+		self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+
+
 class SignUpForm(UserCreationForm):
 	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
 	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
