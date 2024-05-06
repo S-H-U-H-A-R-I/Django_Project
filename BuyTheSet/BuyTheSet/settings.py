737b7c113b43694b1b2f0aa5cb49b0ee4d6e66e2
S-H-U-H-A-R-I <triston.c.0186@gmail.com>
2024-05-06 02:27:55 +0200

    .

--- BuyTheSet/BuyTheSet/settings.py
+++ BuyTheSet/BuyTheSet/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '192.168.0.3']
 
 
 # Application definition
